b9e26ce5dec54db96e03a8079159c757851bbe8b
Iago Bruno <iagobru1258@gmail.com>
2025-02-24 14:21:20 -0300

    Ajustando dados para o swarm

--- helpers/enviroment/__init__.py
+++ helpers/enviroment/__init__.py
@@ -3,5 +3,5 @@ import os
 
 load_dotenv(".env")
 
-URL_SOLR=os.getenv("URL_SOLR", "http://localhost:8983/solr/ocupacoes")
-URL_DB=os.getenv("URL_DB", "postgresql://postgres:12345678@postgres_container:5432/db_ocupacoes")
+URL_SOLR=os.getenv("URL_SOLR", "http://solr:8983/solr/ocupacoes")
+URL_DB=os.getenv("URL_DB", "postgresql://postgres:12345678@database:5432/db_ocupacoes")
